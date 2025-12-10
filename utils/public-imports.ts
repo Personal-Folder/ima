6f0815f07cd3aed74e8ceff0a83aa742acfda583
Youssof-Antar <youssof.antar@gmail.com>
2025-12-10 10:31:58 +0200

    Update public-imports.ts

--- utils/public-imports.ts
+++ utils/public-imports.ts
@@ -1,10 +1,10 @@
-export const logo = "logo.svg";
-export const headerPhoto = "backImage.svg";
-export const heart = "heart.svg";
-export const docLog = "doclog.svg";
+export const logo = "/logo.svg";
+export const headerPhoto = "/backImage.svg";
+export const heart = "/heart.svg";
+export const docLog = "/doclog.svg";
 
-export const discover = "discover.svg";
-export const ambulance = "amb.svg";
+export const discover = "/discover.svg";
+export const ambulance = "/amb.svg";
 
-export const missionImg = "mission.svg";
-export const playImg = "play_button.svg";
+export const missionImg = "/mission.svg";
+export const playImg = "/play_button.svg";
